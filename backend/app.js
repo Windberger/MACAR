27984ea56d2abe78949fa3a0b7e0fbc3795f37f1
Mario Windberger <winmaa21@htl-kaindorf.at>
2024-10-02 20:39:53 +0200

    Backend installieren

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const port = 3000;
+app.use(cors({
+  origin: '*', // Erlaubt alle Urspr�nge
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+/*
+    pm2 start backend
+    pm2 reload backend
+    pm2 stop backend
+*/
