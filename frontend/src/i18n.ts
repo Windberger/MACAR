df575ce5fefd7b01242b4c70999e53a72ce19e63
Winmaa21 <winmaa21@htl-kaindorf.at>
2024-10-29 18:24:41 +0100

    Language Context

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+
+i18n.use(initReactI18next).init({
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+        escapeValue: false,
+    },
+    resources: {
+        en: {
+            translation: {
+                title: "Hello",
+            }
+        },
+        bs: {
+            translation: {
+                title: "Hallo",
+            }
+        }
+    },
+});
+
+
+export default i18n;
