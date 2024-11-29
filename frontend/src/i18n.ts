ca4e628d4ea12bb609070e66de52bbd95ebac72f
Johanna Hechtl <hecjoa21@htl-kaindorf.at>
2024-11-29 09:53:46 +0100

    CompanyPage update

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -27,6 +27,10 @@ i18n.use(initReactI18next).init({
                 secondInformationCardTitle: "Motor vehicle; workshop",
                 secondInformationCardContent: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elit",
 
+
+                // CompanyPage
+                headingCompanyPage: "Our Company"
+
             }
         },
         bs: {
@@ -46,6 +50,9 @@ i18n.use(initReactI18next).init({
                 firstInformationCardContent: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elit",
                 secondInformationCardTitle: "Werkstatt",
                 secondInformationCardContent: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elit",
+
+                // CompanyPage
+                headingCompanyPage: "Unser Unternehmen"
             }
         }
     },
---
+++ frontend/src/pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,83 @@
+import {useState} from "react";
+import heroBackground from "../../assets/images/heroImageCar.png";
+import PropTypes from "prop-types";
+
+
+CompanyPage.propTypes = {
+    heading: PropTypes.string.isRequired
+};
+
+
+function CompanyPage(heading: { heading: string }) {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const images = [heroBackground, heroBackground, heroBackground, heroBackground, heroBackground];
+
+    return (
+
+        <>
+            <h1 className={"font font-bold mt-5 mb-5"}>{heading.heading}</h1>
+
+
+            <div id="default-carousel" className="relative w-full" data-carousel="slide">
+                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className={`duration-700 ease-in-out ${index === activeIndex ? "block" : "hidden"}`}
+                        >
+                            <img
+                                src={image}
+                                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                                alt={`Slide ${index + 1}`}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    {images.map((_, index) => (
+                        <button
+                            key={index}
+                            type="button"
+                            className={`w-3 h-3 rounded-full ${index === activeIndex ? "bg-white" : " bg-opacity-40"}`}
+                            aria-current={index === activeIndex}
+                            aria-label={`Slide ${index + 1}`}
+                            onClick={() => setActiveIndex(index)}
+                        ></button>
+                    ))}
+                </div>
+                <div
+                    className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    onClick={() => setActiveIndex((activeIndex - 1 + images.length) % images.length)}
+                >
+                <span
+                    className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-transparent group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                    <svg className="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M5 1 1 5l4 4"/>
+                    </svg>
+                    <span className="sr-only">Previous</span>
+                </span>
+                </div>
+                <div
+                    className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    onClick={() => setActiveIndex((activeIndex + 1) % images.length)}
+                >
+                <span
+                    className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-transparent group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                    <svg className="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span className="sr-only">Next</span>
+                </span>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default CompanyPage;
