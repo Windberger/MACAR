ff800970cffa60897307389226161e24f1105e9d
Johanna Hechtl <hecjoa21@htl-kaindorf.at>
2024-12-12 18:50:40 +0100

    new InformationCards

--- frontend/src/components/InformationCard.tsx
+++ frontend/src/components/InformationCard.tsx
@@ -1,32 +1,44 @@
-import {Card, CardContent, CardHeader} from "@mui/material";
+import { Card, CardContent, CardHeader } from "@mui/material";
+import { IoCarSport } from "react-icons/io5";
+import { FaShop } from "react-icons/fa6";
 
 interface IInformationCard {
-    title: string,
-    description: string,
-    left: boolean
+    title: string;
+    description: string;
+    left: boolean;
 }
 
-function InformationCard({title, description, left}: IInformationCard) {
-    const styleHeader = "text-black  mt-5 text-left " + (left ? "text-left" : "text-right");
-
-    const parts= title.split(";");
+function InformationCard({ title, description, left }: IInformationCard) {
+    const styleHeader = "text-white mt-5 " + (left ? "text-left" : "text-right");
+    const parts = title.split(";");
 
     return (
-        <Card className="card-glow bg-card  mt-5 mb-4 ml-4 w-[450px] h-[500px] p-5" style={{borderRadius: "30px"}}>
+        <Card
+            className="bg-card mt-5 mb-4 w-full h-full p-5 border-transparent"
+            style={{ borderRadius: "30px" }}
+            elevation={0}
+        >
             <CardHeader
                 className={styleHeader}
                 title={
-                    <div className="text-3xl font-bold">
-                        {parts[0]}
-                        <br/>
-                        {parts[1]}
-                    </div>
+                    <>
+                        <div className="text-3xl font-bold flex flex-col items-center mb-5">
+                            <div
+                                className="mb-4 icon-glow bg-opacity-100 border rounded-3xl border-transparent p-4 flex justify-center">
+                                {left ? <IoCarSport size={48} /> : <FaShop size={48} />}
+                            </div>
+                        </div>
+                        <div className={"text-3xl font-bold flex flex-col"}>
+                            {parts[0]}
+                            <br />
+                            {parts[1]}
+                            <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-red-800" />
+                        </div>
+                    </>
                 }
             />
-            <CardContent className="text-black mb-4">
-                <div className="text-xl text-justify">
-                    {description}
-                </div>
+            <CardContent className="text-white mb-4">
+                <div className="text-xl text-justify">{description}</div>
             </CardContent>
         </Card>
     );
--- frontend/src/pages/InformationCards/InformationCardsLayout.tsx
+++ frontend/src/pages/InformationCards/InformationCardsLayout.tsx
@@ -6,8 +6,8 @@ import InformationCard from "../../components/InformationCard.tsx";
 function InformationCardsLayout() {
     const {t} = useTranslation();
     return (
-        <div className="flex justify-center items-center h-screen mb-5 bg-red-950">
-            <div className="flex space-x-60">
+        <div className="flex justify-center items-center h-screen mb-5 bg-gray">
+            <div className="flex space-x-60 w-4/5 h-4/5 pl-20 pr-20 ">
                 <InformationCard left = {true} title={t("firstInformationCardTitle")} description={t("firstInformationCardContent")}/>
                 <InformationCard left={false} title={t("secondInformationCardTitle")} description={t("secondInformationCardContent")}/>
             </div>
