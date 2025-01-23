4111ea61d5d69553fbcc92219c920fdaa915a997
Johanna Hechtl <hecjoa21@htl-kaindorf.at>
2025-01-23 14:45:14 +0100

    EmailRegistration

--- backend/utils/verificationEmail.ts
+++ backend/utils/verificationEmail.ts
@@ -2,7 +2,8 @@ import * as nodemailer from 'nodemailer';
 
 export async function sendEmailVerification(customerMail: string, token: string) {
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: 'Yahoo',
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
